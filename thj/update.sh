0316267109fd3069f6a301923c142e98ce7d1220
Joseph Harry <findarato@gmail.com>
2025-09-07 12:57:41 -0500

    :bug: fix(update.sh): Fixed a typo Signed-off-by: Joseph Harry <findarato@gmail.com>

--- thj/update.sh
+++ thj/update.sh
@@ -1,4 +1,4 @@
 rsync --inplace -avz --no-perms --no-group --numeric-ids --progress /mnt/storage/everquest_thj/*.ini .
 git add .
 git commit -sm "Updating Configs"
-git pull; git submodule update --remote; git push; git push --tags'
+git pull; git submodule update --remote; git push; git push --tags
